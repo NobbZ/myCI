8b791c27c686352f8391ab0aafee6d1f22c93236
Norbert Melzer <timmelzer@gmail.com>
2013-10-01 09:55:13 +0200

    Sign-In-Form

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -4,6 +4,6 @@ When(/^he logs in with password "(.*?)"$/) do |password|
   visit login_path
   fill_in :name, with: @user.name
   fill_in :password, with: password
-  click :login
+  click_on :login
 end
 
---
+++ spec/views/sessions/new.html.haml_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe 'sessions/new' do
+  it 'should show a loginform' do
+    render
+    puts rendered
+    rendered.should have_selector 'form#login'
+    rendered.should have_selector 'input#name[type=text]'
+    rendered.should have_selector 'input#password[type=password]'
+    rendered.should have_selector 'input#login[type=submit]'
+  end
+end
