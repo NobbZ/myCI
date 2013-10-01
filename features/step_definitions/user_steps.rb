22b5ab341cbf0a65d2a17d2f79796b2a61eaa478
Norbert Melzer <timmelzer@gmail.com>
2013-10-01 09:34:11 +0200

    .

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -2,7 +2,7 @@ require 'fabrication'
 
 When(/^he logs in with password "(.*?)"$/) do |password|
   visit login_path
-  fill_in :user_name, with: @user.name
+  fill_in :name, with: @user.name
   fill_in :password, with: password
   click :login
 end
