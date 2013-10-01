53aea56d692462f0e73d4deb9f6de10b22cc136c
Norbert Melzer <timmelzer@gmail.com>
2013-10-01 13:48:11 +0200

    User is already persisted?
    Something going wrong here…
    But just lets continue with testing!

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,15 +1,19 @@
 require 'fabrication'
 
+def login(username, password)
+  visit login_path
+  fill_in :name, with: username
+  fill_in :password, with: password
+  click_on :login
+end
+
 Given(/^the following User\-template$/) do |table|
   user_data = table.rows_hash
   @user = Fabricate.build :user, user_data
 end
 
 When(/^he logs in with password "(.*?)"$/) do |password|
-  visit login_path
-  fill_in :name, with: @user.name
-  fill_in :password, with: password
-  click_on :login
+  login(@user.name, password)
 end
 
 When(/^he signs up$/) do
@@ -23,5 +27,10 @@ end
 
 Then(/^(I|he) should see "(.*?)"$/) do |x, expected_text|
   should have_content expected_text
+end
+
+Then(/^he should be able to sign in$/) do
+  login(@user.name, @user.password)
+  step "he should see \"Welcome #{@user.name}\""
 end
 
