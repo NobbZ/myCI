17c4738a06f684b676d8e26709d2df5d92bc296b
Norbert Melzer <timmelzer@gmail.com>
2013-10-01 15:41:24 +0200

    Refactored sessions_controller_spec

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,20 +2,24 @@ require 'spec_helper'
 
 describe SessionsController do
   describe '#new' do
+    before(:each) { get :new }
+
+    it { should render_template :new }
   end
 
   describe '#create' do
+    before(:each) { post :create, {name: user.name, password: user.password} }
+
     context 'with existing user' do
-      it 'should set current_user' do
-        user = Fabricate :user
-        post :create, {name: user.name, password: user.password}
-        expect(session[:current_user]).to eq user.id
-      end
+      let(:user) { Fabricate :user }
+
+      it { session[:current_user].should eq user.id }
+
+      it { flash.to_hash.should have_key :notice }
     end
 
     context 'with not existing user' do
       let(:user) { Fabricate.build :user }
-      before(:each) { post :create, {name: user.name, password: user.password} }
 
       it { flash.to_hash.should have_key :alert }
 
