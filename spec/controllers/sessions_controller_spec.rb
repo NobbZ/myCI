124eb286e158fa1511ef82d141444bc263a6f556
Norbert Melzer <timmelzer@gmail.com>
2013-10-02 09:15:30 +0200

    Refactered logout-specs

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -34,13 +34,13 @@ describe SessionsController do
   end
 
   describe '#destroy' do
-    it 'should delete current user session' do
-      user = Fabricate :user
-      delete :destroy, nil, {current_user: user.id}
-      flash.to_hash.should have_key :notice
-      flash[:notice].should eq 'Logged out successfully'
-      session[:current_user].should be_nil
-      should redirect_to root_path
-    end
+    let(:user) { Fabricate :user }
+
+    before(:each) { delete :destroy, nil, {current_user: user.id} }
+
+    it { flash.to_hash.should have_key :notice }
+    it { flash[:notice].should eq 'Logged out successfully' }
+    it { session[:current_user].should be_nil }
+    it { should redirect_to root_path }
   end
 end
